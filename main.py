e0f44d1ee69b943ccc6958afbd9228a6107cfbd1
Полина Полянская <ppolina353@yandex.ru>
2022-07-23 23:33:34 +0300

    рисует квадрат

---
+++ main.py
@@ -0,0 +1,17 @@
+import turtle
+
+t = turtle.Turtle()  
+
+t.color("blue")
+
+t.forward(100)
+t.left(90) 
+t.forward(100)
+t.left(90)
+t.forward(100)
+t.left(90)
+t.forward(100)
+t.left(90)
+
+turtle.done() 
+
